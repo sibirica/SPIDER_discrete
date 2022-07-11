0835971c5264a85b7df08fb02c680210720e6eda
Carlos Silva <kakarotocm@hotmail.com>
2022-07-11 15:26:05 -0300

    Revert "Revert "Merge branch 'carlos' into stable""
    This reverts commit 61a06946135d44b936d8ed103e6eeab958bade30.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SPIDER_discrete
- Generalized sparse regression for continuous and discrete data
+
+Generalized sparse regression for continuous and discrete data
--- discrete/convolution.py
+++ discrete/convolution.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 
+
 # probably won't be used
 def coarse_grain(binned, sigma, truncate=8, wrap=True):
-    sigma_vector = np.zeros(shape=(len(binned.shape),1))
+    sigma_vector = np.zeros(shape=(len(binned.shape), 1))
     sigma_vector[0:2] = sigma
     if wrap:
         smoothed = gaussian_filter(binned, sigma_vector, mode='wrap', truncate=truncate)
@@ -11,20 +12,21 @@ def coarse_grain(binned, sigma, truncate=8, wrap=True):
         smoothed = gaussian_filter(binned, sigma_vector, truncate=truncate)
     return smoothed
 
+
 # consider using square-cubed kernel which has 3 derivatives vanishing on edges (see Wikipedia KDE)
 def gauss1d(x0, sigma, truncate=8, xmin=-np.inf, xmax=np.inf, wrap=False):
     # truncate = 6 or 8 should be fine
     offset = int(x0)
-    s22 = 2*sigma*sigma
-    halfwidth = int(np.ceil(truncate*sigma))
+    s22 = 2 * sigma * sigma
+    halfwidth = int(np.ceil(truncate * sigma))
     if not wrap:
-        mn = np.max([offset-halfwidth, xmin])
-        mx = np.min([offset+halfwidth, xmax])+1 # so that range includes mx
+        mn = np.max([offset - halfwidth, xmin])
+        mx = np.min([offset + halfwidth, xmax]) + 1  # so that range includes mx
     else:
-        mn = offset-halfwidth 
-        mx = offset+halfwidth+1
+        mn = offset - halfwidth
+        mx = offset + halfwidth + 1
     x = np.arange(mn, mx)
-    gx = np.exp(-(x-x0)*(x-x0)/(s22))
-    #gx /= ((2*np.pi)**0.5*sigma) # (normalize approximately)
-    gx /= sum(gx) 
+    gx = np.exp(-(x - x0) * (x - x0) / s22)
+    # gx /= ((2*np.pi)**0.5*sigma) # (normalize approximately)
+    gx /= sum(gx)
     return gx, mn, mx
