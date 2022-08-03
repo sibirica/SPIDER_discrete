Critical fixes to (1) convolution, (2) normalization of errors & columns
- kernel normalization was incorrect
- derivatives were incorrectly accounted in column norms
- error for rank 1 library used first column, not "1" column

continuous/sparse_reg.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def sparse_reg(Theta, opts=None, threshold='pareto', brute_force=True, delta=1e-10, epsilon=1e-2, gamma=2,
-               verbose=False, n_terms=-1, char_sizes=None, row_norms=None, valid_single=None, avoid=[], subinds=None, anchor_col=0):
+               verbose=False, n_terms=-1, char_sizes=None, row_norms=None, valid_single=None, avoid=[], subinds=None, anchor_norm=None):
 # compute sparse regression on Theta * Xi = 0
 # Theta: matrix of integrated terms
 # char_sizes: vector of characteristic term sizes (per column)
@@ -21,13 +21,13 @@ def sparse_reg(Theta, opts=None, threshold='pareto', brute_force=True, delta=1e-
             Theta[row, :] *= row_norms[row]       
     if char_sizes is not None: # do this here: char_sizes are indexed by full column set
         char_sizes = np.array(char_sizes)
-        char_sizes /= np.max(char_sizes)
+        #char_sizes /= np.max(char_sizes)
         for term in range(len(char_sizes)):
             Theta[:, term] = Theta[:, term] / char_sizes[term] # renormalize by characteristic size
-    if anchor_col is None: # do this exactly here: when we divide by Thetanm later, we work with the normalized columns
+    if anchor_norm is None: # do this exactly here: when we divide by Thetanm later, we work with the normalized columns
         Thetanm = np.linalg.norm(Theta)
-    else: # do this here: anchor_col is also indexed by full columns set
-        Thetanm = np.linalg.norm(Theta[:, anchor_col])
+    else:
+        Thetanm = np.linalg.norm(Theta[:, anchor_norm])
 
     if subinds is not None:
         if subinds == []: # no inds allowed at all
@@ -46,7 +46,7 @@ def sparse_reg(Theta, opts=None, threshold='pareto', brute_force=True, delta=1e-
         Theta = np.vstack([Theta, M*np.transpose(Xi)]) # acts as a constraint - weights should be orthogonal to Xi
     
     h, w = Theta.shape
-    if anchor_col is None:
+    if anchor_norm is None:
         Thetanm /= np.sqrt(w) # scale norm of Theta by square root of # columns to fix scaling of Theta@Xi vs Thetanm
     #beta = w/h # aspect ratio
     

discrete/read_LAMMPS.py
@@ -85,7 +85,7 @@ def dump_to_traj(in_file, out_file, num_dimensions, timestep, vel_file=None):
                     atom = int(datafields[0])-1
                     v = np.array(datafields[1:1+num_dimensions])
                     v_slice[atom, :] = v
-        vs = np.dstack(v_list)
+        vs = np.dstack(v_list)/2 # for some reason the computed velocities are off by a factor of 2
     # save trajectories, velocities, & dt to out_file
     with open(out_file, 'wb') as f_out:
         np.save(f_out, trajs, allow_pickle=True)