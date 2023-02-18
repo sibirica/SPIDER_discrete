2d6404febe5a1008fbbad0462094e13a87278d08
Carlos Silva <kakarotocm@hotmail.com>
2023-02-17 19:44:41 -0500

    Update weight.py

--- commons/weight.py
+++ commons/weight.py
@@ -1,9 +1,14 @@
 import numpy as np
 from numpy import polynomial as p
+from numbers import Real
 
 
-def weight_leg(q):
-    # return qth Legendre polynomial as a polynomial object
+def weight_leg(q: int) -> p.Polynomial:
+    """
+    Returns qth Legendre polynomial as a polynomial object.
+    :param q: order of Legendre polynomial.
+    :return: qth Legendre polynomial as a polynomial object.
+    """
     orders = np.zeros(q + 1)
     orders[q] = 1
     leg = p.legendre.leg2poly(orders)
@@ -11,12 +16,24 @@ def weight_leg(q):
     return poly
 
 
-def weight_poly(m):
-    # return (x^2-1)^m as a polynomial object
+def weight_poly(m: int) -> p.Polynomial:
+    """
+    Returns (x^2-1)^m as a polynomial object.
+    :param m: Power of polynomial.
+    :return: (x^2-1)^m as a polynomial object.
+    """
     return p.Polynomial([-1, 0, 1]) ** m
 
 
-def weight_1d(m, q, k, dx):
+def weight_1d(m: int, q: int, k: int, dx: Real) -> p.Polynomial:
+    """
+    Returns the kth derivative of the 1D weight function.
+    :param m: Power of (x^2-1) polynomial.
+    :param q: Order of Legendre polynomial.
+    :param k: Order of derivative.
+    :param dx: Grid spacing.
+    :return: kth derivative of the 1D weight function.
+    """
     poly = weight_leg(q) * weight_poly(m)
     poly = poly / dx ** k
     return poly.deriv(k)
