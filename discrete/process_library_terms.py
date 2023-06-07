781518b52af5d98d8c5a7510cd1536f29c026e7d
Carlos Silva <kakarotocm@hotmail.com>
2023-06-07 17:33:17 -0300

    Introducing Coarse Graining with Numba and KD Trees
    also small changes in typing and indentation.

--- discrete/process_library_terms.py
+++ discrete/process_library_terms.py
@@ -1,13 +1,21 @@
-import numpy as np
-import copy
 from operator import mul
 import scipy
 from findiff import FinDiff
 from commons.weight import *
 from convolution import *
 from library import *
-from scipy.stats import gaussian_kde
 from scipy.stats._stats import gaussian_kernel_estimate
+from coarse_grain_utils import coarse_grain_time_slices
+from operator import mul
+
+import scipy
+from findiff import FinDiff
+from scipy.stats._stats import gaussian_kernel_estimate
+
+from commons.weight import *
+from convolution import *
+from library import *
+
 
 # if we want to use integration domains with different sizes & spacings, it might be
 # better to store that information within this object as well
@@ -121,10 +129,10 @@ class SRDataset(object):  # structures all data associated with a given sparse r
         self.domain_neighbors = None
         self.cutoff = cutoff  # how many std deviations to cut off gaussians at
         self.scale_dict = None  # dict of characteristic scales of observables -> (mean, std)
-        self.xscale = 1 # length scale for correct computation of char scales (default 1)
-        self.tscale = 1 # time scale
+        self.xscale = 1  # length scale for correct computation of char scales (default 1)
+        self.tscale = 1  # time scale
         # use only if interpolation is moved into SRDataset functionality (i.e. on specific domains only)
-        #self.interp_factor = 1 # subsampling factor by which to interpolate positions/data in time
+        # self.interp_factor = 1 # subsampling factor by which to interpolate positions/data in time
 
     def make_libraries(self, max_complexity=4, max_observables=3, max_rho=999):
         self.libs = dict()
@@ -135,9 +143,11 @@ class SRDataset(object):  # structures all data associated with a given sparse r
 
     def make_domains(self, ndomains, domain_size, pad=0):
         self.domains = []
-        scaled_dims = [int(s * self.cg_res) for s in domain_size[:-1]] + [domain_size[-1]] #self.interp_factor *
-        scaled_world_size = [int(s * self.cg_res) for s in self.world_size[:-1]] + [self.world_size[-1]] # self.interp_factor *
-        pads = [np.ceil(pad*self.cg_res) for s in domain_size[:-1]] + [0] # padding by pad in original units on spatial dims
+        scaled_dims = [int(s * self.cg_res) for s in domain_size[:-1]] + [domain_size[-1]]  # self.interp_factor *
+        scaled_world_size = [int(s * self.cg_res) for s in self.world_size[:-1]] + [
+            self.world_size[-1]]  # self.interp_factor *
+        pads = [np.ceil(pad * self.cg_res) for s in domain_size[:-1]] + [
+            0]  # padding by pad in original units on spatial dims
         self.domain_size = scaled_dims
         for i in range(ndomains):
             min_corner = []
@@ -148,9 +158,9 @@ class SRDataset(object):  # structures all data associated with a given sparse r
                 min_corner.append(num)
                 max_corner.append(num + L - 1)
             # (potentially) less messy if we fix beginning/end of time extent to the actual measurements
-            #time_fraction = min_corner % self.interp_factor
-            #min_corner -= time_fraction
-            #max_corner -= time_fraction
+            # time_fraction = min_corner % self.interp_factor
+            # min_corner -= time_fraction
+            # max_corner -= time_fraction
             self.domains.append(IntegrationDomain(min_corner, max_corner))
 
     def find_domain_neighbors(self):
@@ -206,78 +216,107 @@ class SRDataset(object):  # structures all data associated with a given sparse r
         return product
 
     # use only if interpolation is moved into SRDataset functionality (i.e. on specific domains only)
-    #def set_interp_factor(self, factor):
+    # def set_interp_factor(self, factor):
     #    self.interp_factor = factor
     #    self.dxs[-1] /= factor
-    
-    def eval_cgp(self, cgp, obs_dims, domain, experimental=True):
-        data_slice = np.zeros(domain.shape)
-        if self.domain_neighbors is None:
-            self.find_domain_neighbors()
-        for t in range(domain.shape[-1]):
-            time_slice = np.zeros(domain.shape[:-1])
-            t_shifted = t + domain.min_corner[-1]
-            if experimental:
-                # experimental method using scipy.stats.gaussian_kde
-                particles = self.domain_neighbors[domain, t_shifted]
-                pt_pos = self.scaled_pts[particles, :, t_shifted]/self.cg_res
-                weights = np.ones_like(particles, dtype=np.float)
-                obs_dim_ind = 0
-                for obs in cgp.obs_list:
-                    if obs.rank == 0:
-                        data = self.data_dict[obs.string][particles, 0, t_shifted]
-                    else:
-                        data = self.data_dict[obs.string][particles, obs_dims[obs_dim_ind], t_shifted]
-                    weights *= data.astype(np.float)
-                    obs_dim_ind += obs.rank
-                sigma = self.scaled_sigma**2 / (self.cg_res**2)
-                # Check scipy version. If it's lower than 1.10, use inverse_covariance, otherwise use Cholesky
-                if int(scipy.__version__.split(".")[0]) <= 1 and int(scipy.__version__.split(".")[1]) < 10:
-                    inv_cov = np.eye(2) / sigma
-                else:
-                    inv_cov = np.eye(2) * sigma
-                    inv_cov = np.linalg.cholesky(inv_cov[::-1, ::-1]).T[::-1, ::-1]
-                min_corner = domain.min_corner[:-1]
-                max_corner = domain.max_corner[:-1]
-                xx, yy = np.mgrid[min_corner[0]:(max_corner[0] + 1), min_corner[1]:(max_corner[1] + 1)]
-                positions = np.vstack([(xx / self.cg_res).ravel(), (yy / self.cg_res).ravel()]).T
-                density = gaussian_kernel_estimate['double'](pt_pos, weights[:, None], positions, inv_cov,
-                                                             np.float)
-                time_slice = np.reshape(density[:, 0], xx.shape)
 
-                data_slice[..., t] = time_slice/(self.cg_res**2)
-            else:
-                for i in self.domain_neighbors[domain, t_shifted]:
-                    pt_pos = self.scaled_pts[i, :, t_shifted]
-                    # evaluate observables inside rho[...]
-                    coeff = 1
+    def eval_cgp(self,
+                 cgp: CoarseGrainedPrimitive,
+                 obs_dims,
+                 domain: IntegrationDomain,
+                 experimental: bool = True):
+        data_slice = np.zeros(domain.shape)
+        if experimental:
+            pt_pos = self.scaled_pts[:, :, domain.times] / self.cg_res  # Unscaled positions
+            weights = np.ones_like(pt_pos[:, 0, :], dtype=np.float)
+            obs_dim_ind = 0
+            for obs in cgp.obs_list:
+                if obs.rank == 0:
+                    data = self.data_dict[obs.string][:, 0, domain.times]
+                else:
+                    data = self.data_dict[obs.string][:, obs_dims[obs_dim_ind], domain.times]
+                weights *= data.astype(np.float)
+                obs_dim_ind += obs.rank
+            sigma = self.scaled_sigma ** 2 / (self.cg_res ** 2)
+            min_corner = domain.min_corner[:-1]
+            max_corner = domain.max_corner[:-1]
+            xx, yy = np.mgrid[
+                         min_corner[0]:(max_corner[0] + 1),
+                         min_corner[1]:(max_corner[1] + 1)
+                         ]
+            xi = np.vstack([
+                (xx / self.cg_res).ravel(),
+                (yy / self.cg_res).ravel(),
+            ]).T
+            data_slice = coarse_grain_time_slices(pt_pos, weights, xi, sigma)
+            data_slice = data_slice.reshape(domain.shape)
+        else:
+            if self.domain_neighbors is None:
+                self.find_domain_neighbors()
+            for t in range(domain.shape[-1]):
+                time_slice = np.zeros(domain.shape[:-1])
+                t_shifted = t + domain.min_corner[-1]
+                if experimental:
+                    # experimental method using scipy.stats.gaussian_kde
+                    particles = self.domain_neighbors[domain, t_shifted]
+                    pt_pos = self.scaled_pts[particles, :, t_shifted] / self.cg_res
+                    weights = np.ones_like(particles, dtype=np.float)
                     obs_dim_ind = 0
                     for obs in cgp.obs_list:
-                        # print(obs, i, obs_dims[obs_dim_ind], t_shifted)
                         if obs.rank == 0:
-                            coeff *= self.data_dict[obs.string][i, 0, t_shifted]
+                            data = self.data_dict[obs.string][particles, 0, t_shifted]
                         else:
-                            coeff *= self.data_dict[obs.string][i, obs_dims[obs_dim_ind], t_shifted]
+                            data = self.data_dict[obs.string][particles, obs_dims[obs_dim_ind], t_shifted]
+                        weights *= data.astype(np.float)
                         obs_dim_ind += obs.rank
-                        # print(coeff)
-                    # coarse-graining this particle (one dimension at a time)
-                    rngs = []
-                    g_nd = 1
-                    for coord, d_min, d_max, j in zip(pt_pos, domain.min_corner, domain.max_corner,
-                                                      range(self.n_dimensions - 1)):
-                        # recenter so that 0 is start of domain
-                        g, mn, mx = gauss1d(coord - d_min, self.scaled_sigma, truncate=self.cutoff,
-                                            xmin=0, xmax=d_max - d_min)
-                        g_nd = np.multiply.outer(g_nd, g)
-                        rng_array = np.array(range(mn, mx))  # coordinate range of kernel
-                        # now need to add free axes so that the index ends up as an (n-1)-d array
-                        n_free_dims = self.n_dimensions - j - 2  # how many np.newaxis to add to index
-                        expanded_rng_array = np.expand_dims(rng_array, axis=tuple(range(1, 1 + n_free_dims)))
-                        rngs.append(expanded_rng_array)
-                    # if len((g_nd*coeff).shape) > len(time_slice.shape):
-                    #    print(rngs, g_nd.shape, coeff)
-                    time_slice[tuple(rngs)] += g_nd * coeff
-                data_slice[..., t] = time_slice
+                    sigma = self.scaled_sigma ** 2 / (self.cg_res ** 2)
+                    # Check scipy version. If it's lower than 1.10, use inverse_covariance, otherwise use Cholesky
+                    if int(scipy.__version__.split(".")[0]) <= 1 and int(scipy.__version__.split(".")[1]) < 10:
+                        inv_cov = np.eye(2) / sigma
+                    else:
+                        inv_cov = np.eye(2) * sigma
+                        inv_cov = np.linalg.cholesky(inv_cov[::-1, ::-1]).T[::-1, ::-1]
+                    min_corner = domain.min_corner[:-1]
+                    max_corner = domain.max_corner[:-1]
+                    xx, yy = np.mgrid[min_corner[0]:(max_corner[0] + 1), min_corner[1]:(max_corner[1] + 1)]
+                    positions = np.vstack([(xx / self.cg_res).ravel(), (yy / self.cg_res).ravel()]).T
+                    density = gaussian_kernel_estimate['double'](pt_pos, weights[:, None], positions, inv_cov,
+                                                                 np.float)
+                    time_slice = np.reshape(density[:, 0], xx.shape)
+
+                    data_slice[..., t] = time_slice / (self.cg_res ** 2)
+                else:
+                    for i in self.domain_neighbors[domain, t_shifted]:
+                        pt_pos = self.scaled_pts[i, :, t_shifted]
+                        # evaluate observables inside rho[...]
+                        coeff = 1
+                        obs_dim_ind = 0
+                        for obs in cgp.obs_list:
+                            # print(obs, i, obs_dims[obs_dim_ind], t_shifted)
+                            if obs.rank == 0:
+                                coeff *= self.data_dict[obs.string][i, 0, t_shifted]
+                            else:
+                                coeff *= self.data_dict[obs.string][i, obs_dims[obs_dim_ind], t_shifted]
+                            obs_dim_ind += obs.rank
+                            # print(coeff)
+                        # coarse-graining this particle (one dimension at a time)
+                        rngs = []
+                        g_nd = 1
+                        for coord, d_min, d_max, j in zip(pt_pos, domain.min_corner, domain.max_corner,
+                                                          range(self.n_dimensions - 1)):
+                            # recenter so that 0 is start of domain
+                            g, mn, mx = gauss1d(coord - d_min, self.scaled_sigma, truncate=self.cutoff,
+                                                xmin=0, xmax=d_max - d_min)
+                            g_nd = np.multiply.outer(g_nd, g)
+                            rng_array = np.array(range(mn, mx))  # coordinate range of kernel
+                            # now need to add free axes so that the index ends up as an (n-1)-d array
+                            n_free_dims = self.n_dimensions - j - 2  # how many np.newaxis to add to index
+                            expanded_rng_array = np.expand_dims(rng_array, axis=tuple(range(1, 1 + n_free_dims)))
+                            rngs.append(expanded_rng_array)
+                        # if len((g_nd*coeff).shape) > len(time_slice.shape):
+                        #    print(rngs, g_nd.shape, coeff)
+                        time_slice[tuple(rngs)] += g_nd * coeff
+                    data_slice[..., t] = time_slice
         data_slice *= self.cg_res ** (self.n_dimensions - 1)  # need to scale rho by res^(# spatial dims)!
         return data_slice
 
@@ -329,7 +368,7 @@ class SRDataset(object):  # structures all data associated with a given sparse r
 
     # separated into helper function to simplify external plotting of fields
     def make_tw_arr(self, term, weight, domains, k, by_parts, debug=False):
-        dshape = self.domain_size # we don't currently allow domains to have different shapes, though we could
+        dshape = self.domain_size  # we don't currently allow domains to have different shapes, though we could
         arr = np.zeros(np.append(dshape, len(domains)))
         if isinstance(term, ConstantTerm):
             # "short circuit" the evaluation to deal with constant term case
@@ -368,7 +407,7 @@ class SRDataset(object):  # structures all data associated with a given sparse r
                     arr[..., _p] += self.eval_term(indexed_term, weight, domain,
                                                    debug=(debug and _p == 0))
         return arr
-        
+
     def find_scales(self, names=None):
         # find mean/std deviation of fields in data_dict that are in names
         self.scale_dict = dict()
@@ -387,7 +426,7 @@ class SRDataset(object):  # structures all data associated with a given sparse r
         rho_cgp = self.cgps[rho_ind]
         rho_std = np.std(np.dstack([self.cg_dict[rho_cgp, (), domain] for domain in self.domains]))
         self.scale_dict['rho']['std'] = rho_std
-        
+
     ### TO DO: compute correlation length/time automatically
     def set_LT_scale(self, L, T):
         self.xscale = L
@@ -409,8 +448,8 @@ class SRDataset(object):  # structures all data associated with a given sparse r
             # add in rho contribution (every primitive contains a rho)
             product *= self.scale_dict['rho'][statistic]
             # scale by correlation length (time) / dx (dt)
-            product /= self.xscale**xorder
-            product /= self.tscale**torder
+            product /= self.xscale ** xorder
+            product /= self.tscale ** torder
         return product
 
     def find_row_weights(self):
@@ -450,6 +489,7 @@ def get_slice(arr, domain):
         idx[slice_dim] = slice(min_c, max_c + 1)
         arr_slice = arr_slice[tuple(idx)]
     return arr_slice
+
 
 def get_dims(term, ndims, dim=None, start=0, do=None, od=None):
     # yield all of the possible x, y, z labelings for a given LibraryTerm
