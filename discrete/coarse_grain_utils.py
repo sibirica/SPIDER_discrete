Fixed sigma scaling in coarse grain and added cutoff

discrete/coarse_grain_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import jit, float64, uint64, prange
+from numba import jit, float64, uint64, prange, int64
 from numba_kdtree import KDTree
 
 
@@ -47,7 +47,7 @@ def gaussian_coarse_grain2d(points: float64[:, :],
 
 
 @jit(
-    signature_or_function="float64[:](float64[:, :], float64[:], float64[:, :], float64)",
+    signature_or_function="float64[:](float64[:, :], float64[:], float64[:, :], float64, float64)",
     nopython=True,
     cache=False,
     fastmath=False,
@@ -59,7 +59,8 @@ def gaussian_coarse_grain2d(points: float64[:, :],
 def kd_gaussian_coarse_grain2d(points: float64[:, :],
                                values: float64[:],
                                xi: float64[:, :],
-                               sigma: float64) -> float64[:]:
+                               sigma: float64,
+                               cutoff: float64) -> float64[:]:
     """
     This function implements a gaussian coarse graining algorithm. Uses a KDTree to only consider nearby points.
     Heavily inspired by the scipy implementation at https://github.com/scipy/scipy/blob/main/scipy/stats/_stats.pyx
@@ -67,6 +68,7 @@ def kd_gaussian_coarse_grain2d(points: float64[:, :],
     :param values: the multivariate values associated with the data points. (n,)
     :param xi: the coordinates to evaluate the estimate at in 2 dimensions. Shape (m, 2).
     :param sigma: the gaussian kernel width (standard deviation). Float.
+    :param cutoff: the cutoff radius for the KDTree in units of sigma. Float.
     :return: the coarse grained data at the coordinates xi. Shape (m,).
     """
     n: uint64 = points.shape[0]  # number of data points
@@ -83,7 +85,7 @@ def kd_gaussian_coarse_grain2d(points: float64[:, :],
     norm: float64 = 1 / (2 * np.pi) / (sigma * sigma)  # the normalization factor of the gaussian kernel
 
     for j in prange(m):
-        neighbors: uint64[:] = tree.query_radius(xi_[j, :], 10)[0]
+        neighbors: uint64[:] = tree.query_radius(xi_[j, :], cutoff)[0]
         for i in neighbors:
             estimate[j] += np.exp(-np.sum((points_[i, :] - xi_[j, :]) * (points_[i, :] - xi_[j, :])) / 2) \
                            * values[i]
@@ -92,7 +94,7 @@ def kd_gaussian_coarse_grain2d(points: float64[:, :],
 
 
 @jit(
-    signature_or_function="float64[:,:](float64[:, :, :], float64[:, :], float64[:, :], float64)",
+    signature_or_function="float64[:,:](float64[:, :, :], float64[:, :], float64[:, :], float64, float64)",
     nopython=True,
     cache=False,
     fastmath=False,
@@ -104,7 +106,8 @@ def kd_gaussian_coarse_grain2d(points: float64[:, :],
 def coarse_grain_time_slices(points: float64[:, :, :],
                              values: float64[:, :],
                              xi: float64[:, :],
-                             sigma: float64) -> float64[:, :]:
+                             sigma: float64,
+                             cutoff: float64) -> float64[:, :]:
     """
     This function implements a gaussian coarse graining algorithm. Uses a KDTree to only consider nearby points.
     Heavily inspired by the scipy implementation at https://github.com/scipy/scipy/blob/main/scipy/stats/_stats.pyx
@@ -112,6 +115,7 @@ def coarse_grain_time_slices(points: float64[:, :, :],
     :param values: the multivariate values associated with the data points. (n, t)
     :param xi: the coordinates to evaluate the estimate at in 2 dimensions. Shape (m, 2).
     :param sigma: the gaussian kernel width (standard deviation). Float.
+    :param cutoff: the cutoff radius for the KDTree in units of sigma. Float.
     :return: the coarse grained data at the coordinates xi. Shape (m, t).
     """
     m: uint64 = xi.shape[0]  # number of evaluation points
@@ -119,6 +123,61 @@ def coarse_grain_time_slices(points: float64[:, :, :],
 
     estimate: float64[m, t] = np.zeros((m, t))  # the estimate at the evaluation points
     for h in range(t):
-        estimate[:, h] = kd_gaussian_coarse_grain2d(points[:, :, h], values[:, h], xi[:, :], sigma)
+        estimate[:, h] = kd_gaussian_coarse_grain2d(points[:, :, h], values[:, h], xi[:, :], sigma, cutoff)
 
     return estimate
+
+
+# WARNING: CURRENTLY DOESN'T WORK WITH PERIODIC BOUNDARIES
+@jit(
+    signature_or_function="float64[:,:](float64[:, :, :], float64[:, :], float64[:, :], float64, float64, int64)",
+    nopython=True,
+    cache=False,
+    fastmath=False,
+    parallel=True,
+    debug=False,
+    nogil=True,
+    boundscheck=True
+)
+def verlet_coarse_grain_time_slices(points: float64[:, :, :],
+                                    values: float64[:, :],
+                                    xi: float64[:, :],
+                                    sigma: float64,
+                                    cutoff: float64,
+                                    h: int64) -> float64[:, :]:
+    """
+    This function implements a gaussian coarse graining algorithm. Uses a KDTree to only consider nearby points.
+    Heavily inspired by the scipy implementation at https://github.com/scipy/scipy/blob/main/scipy/stats/_stats.pyx
+    :param points: the data points to estimate from in 2 dimensions + time. Shape (n, 2, t).
+    :param values: the multivariate values associated with the data points. (n, t)
+    :param xi: the coordinates to evaluate the estimate at in 2 dimensions. Shape (m, 2).
+    :param sigma: the gaussian kernel width (standard deviation). Float.
+    :param cutoff: the cutoff radius for the KDTree in units of sigma. Float.
+    :param h: How often the verlet list is updated. uint64.
+    :return: the coarse grained data at the coordinates xi. Shape (m, t).
+    """
+    n: uint64 = points.shape[0]  # number of data points
+    m: uint64 = xi.shape[0]  # number of evaluation points
+    t: uint64 = points.shape[2]  # number of time slices
+
+    points_: float64[:, :, :] = points / sigma  # the scaled data points
+    xi_: float64[:, :] = xi / sigma  # the scaled evaluation points
+
+    leaf_size: uint64 = np.floor(np.log2(n))  # the leaf_size of the KDTree (log2(n) is a good heuristic)
+
+    estimate: float64[m, t] = np.zeros((m, t))  # the estimate at the evaluation points
+    norm: float64 = 1 / (2 * np.pi) / (sigma * sigma)  # the normalization factor of the gaussian kernel
+    for k in np.arange(int64(0), t, h):
+        # build the KDTree
+        # print("Building KDTree for time slice " + str(k) + " of " + str(t) + " time slices.")
+        tree = KDTree(points_[:, :, k], leafsize=leaf_size)  # the KDTree of the scaled data points
+        for j in prange(m):
+            neighbors: uint64[:] = tree.query_radius(xi_[j, :], cutoff)[0]
+            for g in range(h):
+                if k + g >= t:
+                    break
+                for i in neighbors:
+                    estimate[j, k + g] += np.exp(-np.sum((points_[i, :, k + g] - xi_[j, :]) *
+                                                         (points_[i, :, k + g] - xi_[j, :])) / 2) * values[i, k + g]
+
+    return estimate * norm

discrete/process_library_terms.py
@@ -237,7 +237,7 @@ class SRDataset(object):  # structures all data associated with a given sparse r
                     data = self.data_dict[obs.string][:, obs_dims[obs_dim_ind], domain.times]
                 weights *= data.astype(np.float)
                 obs_dim_ind += obs.rank
-            sigma = self.scaled_sigma ** 2 / (self.cg_res ** 2)
+            sigma = self.scaled_sigma / self.cg_res
             min_corner = domain.min_corner[:-1]
             max_corner = domain.max_corner[:-1]
             xx, yy = np.mgrid[
@@ -248,7 +248,7 @@ class SRDataset(object):  # structures all data associated with a given sparse r
                 (xx / self.cg_res).ravel(),
                 (yy / self.cg_res).ravel(),
             ]).T
-            data_slice = coarse_grain_time_slices(pt_pos, weights, xi, sigma)
+            data_slice = coarse_grain_time_slices(pt_pos, weights, xi, sigma, self.cutoff)
             data_slice = data_slice.reshape(domain.shape)
         else:
             if self.domain_neighbors is None: