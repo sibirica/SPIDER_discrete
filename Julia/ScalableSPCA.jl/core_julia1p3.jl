58db039c6603b8d6ffe7534bfd5a92e788289aa1
Daniel Gurevich <53491576+sibirica@users.noreply.github.com>
2025-02-11 17:46:48 -0500

    clean up unused and outdated scripts

--- Julia/ScalableSPCA.jl/core_julia1p3.jl
+++
@@ -1,3 +0,0 @@
-using JuMP, Random, LinearAlgebra, JLD, DataFrames, Test, Suppressor, DelimitedFiles, CSV, StatsBase, MosekTools, Compat, Arpack, SCS
-include("sdp_bounds.jl")
-#include("sdp_bounds_minus.jl")
--- commons/utils.py
+++ commons/utils.py
@@ -1,6 +1,19 @@
 from collections.abc import Iterable
 from typing import Any
 import re
+import numpy as np
 
 def regex_find(lst: Iterable[Any], pattern: re.Pattern) -> Iterable[tuple[int, Any]]:
     yield from ((i, x) for i, x in enumerate(lst) if re.fullmatch(pattern, str(x)))
+
+def save(filename, *args):
+    with open(filename, 'wb') as f:
+        for arr in args:
+            np.save(f, arr)
+
+def load(filename, nload):
+    to_load = []
+    with open(filename, 'rb') as f:
+        for i in range(nload):
+            to_load.append(np.load(f))
+    return tuple(to_load)
